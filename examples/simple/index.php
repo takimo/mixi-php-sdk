01e6dd65fc857c5f5927ed643fbabd0373f718b9
takimo(Shinya Takimoto) <subtakimo@gmail.com>
2011-08-05 03:24:57 +0900

    fluxflex

--- examples/simple/index.php
+++ examples/simple/index.php
@@ -4,18 +4,51 @@ require_once('../../src/mixi.php');
 
 $consumer_key = (array_key_exists('HTTP_X_FLX_CONSUMER_KEY', $_SERVER))  ? $_SERVER['HTTP_X_FLX_CONSUMER_KEY'] : CONSUMER_KEY;
 $consumer_secret = (array_key_exists('HTTP_X_FLX_CONSUMER_SECRET', $_SERVER))  ? $_SERVER['HTTP_X_FLX_CONSUMER_SECRET'] : CONSUMER_SECRET;
-
-var_dump($_SERVER);
-var_dump("====");
-var_dump($consumer_key);
-var_dump($consumer_secret);
+$redirect_url = (array_key_exists('HTTP_X_FLX_REDIRECT_URL', $_SERVER))  ? $_SERVER['HTTP_X_FLX_REDIRECT_URL'] : REDIRECT_URL;
 
 $mixi = new Mixi(array(
     'consumer_key' => $consumer_key,
     'consumer_secret' => $consumer_secret,
     'scope' => SCOPE,
     'display' => DISPLAY,
-    'redirect_uri' => REDIRECT_URL
+    'redirect_uri' => $redirect_url
 ));
+
+// get user id
+var_dump($mixi->getUser(true));
+
+// get voice timeline
+var_dump($mixi->api('/voice/statuses/friends_timeline'));
+
+// get message inbox
+var_dump($mixi->api('/messages/@me/@inbox/'));
+
+/*
+// post voice
+var_dump($mixi->api('/voice/statuses', 'POST', array(
+    "status" => "こんにちは、世界!"
+)));
+*/
+
+/*
+// post photo
+function loadImage($file_path)
+{
+    $fp = fopen($file_path,'rb');
+    $size = filesize($file_path);
+    $img  = fread($fp, $size);
+    fclose($fp);
+    return $img;
+}
+
+$file_path = "sample.jpeg";
+$info = GetImageSize("sample.jpeg");
+$result = $mixi->api('/photo/mediaItems/@me/@self/@default', 'POST',
+    loadImage('sample.jpeg'),
+    array(
+        'Content-type: '.$info["mime"]
+    )
+);
+*/
 
 ?>
