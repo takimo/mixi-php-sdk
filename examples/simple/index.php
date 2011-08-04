4fb9a9966fe80f12bffd7ba9ede681cbcbd22fe3
takimo(Shinya Takimoto) <subtakimo@gmail.com>
2011-08-05 03:34:34 +0900

    fluxflex

--- examples/simple/index.php
+++ examples/simple/index.php
@@ -14,5 +14,41 @@ $mixi = new Mixi(array(
     'redirect_uri' => $redirect_url
 ));
 
-var_dump($_SERVER);
+// get user id
+var_dump($mixi->getUser(true));
+
+// get voice timeline
+var_dump($mixi->api('/voice/statuses/friends_timeline'));
+
+// get message inbox
+var_dump($mixi->api('/messages/@me/@inbox/'));
+
+/*
+// post voice
+var_dump($mixi->api('/voice/statuses', 'POST', array(
+    "status" => "こんにちは、世界!"
+)));
+*/
+
+/*
+// post photo
+function loadImage($file_path)
+{
+    $fp = fopen($file_path,'rb');
+    $size = filesize($file_path);
+    $img  = fread($fp, $size);
+    fclose($fp);
+    return $img;
+}
+
+$file_path = "sample.jpeg";
+$info = GetImageSize("sample.jpeg");
+$result = $mixi->api('/photo/mediaItems/@me/@self/@default', 'POST',
+    loadImage('sample.jpeg'),
+    array(
+        'Content-type: '.$info["mime"]
+    )
+);
+*/
+
 ?>
