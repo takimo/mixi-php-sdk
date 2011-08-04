091027b167e924e5ccd6140a67a49a32e118542b
takimo(Shinya Takimoto) <subtakimo@gmail.com>
2011-08-05 03:01:51 +0900

    fluxflex

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
 
-define('CONSUMER_KEY', 'your consumer key');
-define('CONSUMER_SECRET', 'your consumer secret');
+/*
+ * http://developer.mixi.co.jp/connect/mixi_graph_api/services/
+ * FLX_CONSUMER_KEY -> set your consumer key
+ * FLX_CONSUMER_SECRET -> set your consumer secret
+ * FLX_REDIRECT_URL -> set your redirect url
+ */
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
 define('SCOPE', 'r_profile w_voice r_voice w_photo r_message');
 define('DISPLAY', 'pc');
-define('REDIRECT_URL', 'http://example/callback.php');
+define('REDIRECT_URL', '');
 
 ?>
--- examples/simple/index.php
+++ examples/simple/index.php
@@ -2,9 +2,12 @@
 require_once('../../config.php');
 require_once('../../src/mixi.php');
 
+$consumer_key = $_SERVER['HTTP_X_FLX_CONSUMER_KEY'],
+$consumer_secret = $_SERVER['HTTP_X_FLX_CONSUMER_SECRET'],
+
 $mixi = new Mixi(array(
-    'consumer_key' => CONSUMER_KEY,
-    'consumer_secret' => CONSUMER_SECRET,
+    'consumer_key' => (array_key_exists('HTTP_X_FLX_CONSUMER_KEY', $_SERVER))  ? $_SERVER['HTTP_X_FLX_CONSUMER_KEY'] : CONSUMER_KEY,
+    'consumer_secret' => (array_key_exists('HTTP_X_FLX_CONSUMER_SECRET', $_SERVER))  ? $_SERVER['HTTP_X_FLX_CONSUMER_SECRET'] : CONSUMER_SECRET,
     'scope' => SCOPE,
     'display' => DISPLAY,
     'redirect_uri' => REDIRECT_URL
