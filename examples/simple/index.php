ffc52fd8496982ecc58a0103f7b7075f2d6fc74f
takimo(Shinya Takimoto) <subtakimo@gmail.com>
2011-08-05 03:46:02 +0900

    fluxflex

--- examples/simple/index.php
+++ examples/simple/index.php
@@ -2,9 +2,9 @@
 require_once('../../config.php');
 require_once('../../src/mixi.php');
 
-$consumer_key = (array_key_exists('HTTP_X_FLX_CONSUMER_KEY', $_SERVER))  ? $_SERVER['HTTP_X_FLX_CONSUMER_KEY'] : CONSUMER_KEY;
-$consumer_secret = (array_key_exists('HTTP_X_FLX_CONSUMER_SECRET', $_SERVER))  ? $_SERVER['HTTP_X_FLX_CONSUMER_SECRET'] : CONSUMER_SECRET;
-$redirect_url = (array_key_exists('HTTP_X_FLX_REDIRECT_URL', $_SERVER))  ? $_SERVER['HTTP_X_FLX_REDIRECT_URL'] : REDIRECT_URL;
+$consumer_key = (array_key_exists('HTTP_X_FLX_CONSUMER_KEY', $_SERVER)) ? $_SERVER['HTTP_X_FLX_CONSUMER_KEY'] : CONSUMER_KEY;
+$consumer_secret = (array_key_exists('HTTP_X_FLX_CONSUMER_SECRET', $_SERVER)) ? $_SERVER['HTTP_X_FLX_CONSUMER_SECRET'] : CONSUMER_SECRET;
+$redirect_url = (array_key_exists('HTTP_X_FLX_REDIRECT_URL', $_SERVER)) ? $_SERVER['HTTP_X_FLX_REDIRECT_URL'] : REDIRECT_URL;
 
 $mixi = new Mixi(array(
     'consumer_key' => $consumer_key,
--- examples/simple/logout.php
+++ examples/simple/logout.php
@@ -2,12 +2,16 @@
 require_once('../../config.php');
 require_once('../../src/mixi.php');
 
+$consumer_key = (array_key_exists('HTTP_X_FLX_CONSUMER_KEY', $_SERVER)) ? $_SERVER['HTTP_X_FLX_CONSUMER_KEY'] : CONSUMER_KEY;
+$consumer_secret = (array_key_exists('HTTP_X_FLX_CONSUMER_SECRET', $_SERVER)) ? $_SERVER['HTTP_X_FLX_CONSUMER_SECRET'] : CONSUMER_SECRET;
+$redirect_url = (array_key_exists('HTTP_X_FLX_REDIRECT_URL', $_SERVER)) ? $_SERVER['HTTP_X_FLX_REDIRECT_URL'] : REDIRECT_URL;
+
 $mixi = new Mixi(array(
-    'consumer_key' => CONSUMER_KEY,
-    'consumer_secret' => CONSUMER_SECRET,
+    'consumer_key' => $consumer_key,
+    'consumer_secret' => $consumer_secret,
     'scope' => SCOPE,
     'display' => DISPLAY,
-    'redirect_uri' => REDIRECT_URL
+    'redirect_uri' => $redirect_url
 ));
 
 $mixi->clearAllAppData();
